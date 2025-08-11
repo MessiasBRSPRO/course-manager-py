838764b0987aea72cf94e446317ad4537185fd77
unknown <meddisme@gmail.com>
2025-08-11 11:49:24 -0300

    Updating the Logic of Course class pt2

--- Course.py
+++ Course.py
@@ -3,15 +3,20 @@ from Teacher import Teacher
 
 class Course:
     def __init__(self, courseName, teacher):
+        #Only teachers can create a course
         if type(teacher) == Teacher:
             self._courseName = courseName;
             self._classes = [];
             self._students = [];
+            self._teacher = teacher
         else:
             print("only teacher can create a Course")
 
     def getCourseName(self):
         return self._courseName;
+
+    def getTeacher(self):
+        return self._teacher
 
     def getClasses(self):
         return self._classes;
@@ -20,4 +25,4 @@ class Course:
         return self._students;
 
     def toString(self):
-        return f"Course:{self._courseName}, Classes:{self.getClasses()}, Students:{self._students}"
+        return f"Course:{self._courseName}, Classes:{self.getClasses()}, Teacher:{self._teacher.getName()}, Students:{self._students}"
