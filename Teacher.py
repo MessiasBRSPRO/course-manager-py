aa21d5e4e6e418fa3f0463918859fb6b8d0078f1
unknown <meddisme@gmail.com>
2025-08-11 11:27:01 -0300

    Created Teacher class

---
+++ Teacher.py
@@ -0,0 +1,22 @@
+class Teacher:
+
+    def __init__(self, name, matter, graduation):
+        self._name = name;
+        self._matters = matter;
+        self._graduation = graduation;
+        self._teachCourses = [];
+
+    def getName(self):
+        return self._name
+
+    def getMatter(self):
+        return self._matters
+
+    def getGraduation(self):
+        return self._graduation
+
+    def getTeachCourses(self):
+        return self._teachCourses
+
+    def toString(self):
+        return f"Teacher name:{self._name}, matters:{self._matters}, graduation description:{self._graduation}, teach Courses:{self._teachCourses}"
